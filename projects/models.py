b6e4c9f19d3d251bfb1f3330043e4b060a7815d4
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-02-21 17:51:48 -0600

    Following serilization tutorial in django rest framework

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_lenth=30)
+    description = models.TextField()
+    last_updated = models.DateField(auto_now=True)
+
+    # showing blog as an example
+    #blog = models.ForeignKey(Blog)
---
+++ projects/serializers.py
@@ -0,0 +1,24 @@
+from django.forms import widgets
+from rest_framework import serializers
+from projects.models import Project
+
+class ProjectSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=30)
+    description = serializers.TextField()
+    linenos = serializers.BooleanField(required=False)
+
+    def create(self, validated_data):
+        """
+        Create and return a new 'Project' instance, given the validated data.
+        """
+        return Project.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing 'Project' instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
