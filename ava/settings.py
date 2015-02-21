4b4e4823d204116e3428ace9245dc45c1703c5e9
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-02-21 16:30:54 -0600

    successfully set up postgresql

--- ava/settings.py
+++ ava/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'ava.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'avadb',
+        'USER': 'hoff',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
