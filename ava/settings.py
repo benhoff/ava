4e3f263e5c15aba34bb706012db72142522e6a2e
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-02-21 17:21:38 -0600

    Added in Rest framework to settings.py

--- ava/settings.py
+++ ava/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -65,6 +66,14 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
+
+REST_FRAMEWORK = {
+        # Use Django's standard 'django.contrib.auth' permissions,
+        # or allow read-only access for unauthenticated users.
+        'DEFAULT_PERMISSION_CLASSES' : [
+            'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        ]
 }
 
 # Internationalization
