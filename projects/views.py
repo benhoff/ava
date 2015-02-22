b6e366da6a898b91513907ecf4a78f4b666530d4
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-02-21 18:24:51 -0600

    But now seriously, that last commit message

--- projects/views.py
+++ projects/views.py
@@ -13,3 +13,21 @@ class JSONResponse(HttpResponse):
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all projects, or create a new project
+    """
+    if request.method == 'GET':
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ProjectSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serialzier.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
