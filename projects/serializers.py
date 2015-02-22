c01f641435828652e79e9252316f009e63a26bd4
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-02-21 18:02:53 -0600

    serializers now featuring more convient initializers, per the tutorial

--- projects/serializers.py
+++ projects/serializers.py
@@ -3,22 +3,7 @@ from rest_framework import serializers
 from projects.models import Project
 
 class ProjectSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=30)
-    description = serializers.CharField()
-    linenos = serializers.BooleanField(required=False)
-
-    def create(self, validated_data):
-        """
-        Create and return a new 'Project' instance, given the validated data.
-        """
-        return Project.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing 'Project' instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.save()
-        return instance
+    class Meta:
+        model = Project
+        fields = ('id', 'title', 'description')
+    
