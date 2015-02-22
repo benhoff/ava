e8a2e21de7126149b07bec1d5909c96c407ef72d
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-02-21 18:23:45 -0600

    done for today. up to @csrf_exempt portion of tutorial

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from projects.models import Project
+from project.serialzers import ProjectSerializer
 
-# Create your views here.
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
