3077144c5e622ac416b0b92d736437fff2d0f02d
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-02-28 10:35:27 -0600

    Finished tutorial 6, did not fix bugs

--- projects/urls.py
+++ projects/urls.py
@@ -1,33 +1,15 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from projects import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'projects', views.ProjectViewSet)
+router.register(r'users', views.UserViewSet)
 
 # API endpoints
-
 urlpatterns = format_suffix_patterns([
-        url(r'^$', views.api_root),
-        url(r'^projects/$', 
-            views.ProjectList.as_view(), 
-            name='project-list'),
+        url(r'^$', include(router.urls)),
 
-        url(r'^projects/(?P<pk>[0-9]+)/$', 
-            views.ProjectDetail.as_view(),
-            name='project-detail'),
-
-        url(r'^projects/(?P<pk>[0-9]+)/highlights/$',
-            views.ProjectHighlight.as_view(),
-            name='project-highlight'),
-        
-        url(r'^users/$', 
-            views.UserList.as_view(),
-            name='user-list'),
-
-        url(r'^users/?(P<pk>[0-9]+)/$', 
-            views.UserDetail.as_view(),
-            name='user-detail'),
+        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ])
-
-urlpatterns += [
-        url(r'^api-auth/', include('rest_framework.urls',
-                                   namespace='rest_framework'))
-]                           
--- projects/views.py
+++ projects/views.py
@@ -9,7 +9,36 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automagically provides 'list' and 'detail' actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
+class ProjectViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automagically provides 'list', 'create', 'retieve',
+    'update', and 'destroy' actions.
+
+    Additionally we also provide an extra 'highlight' action.
+    """
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(render_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        project = self.get_object()
+        return Response(project.description)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+"""
 
 @api_view(('GET',))
 def api_root(request, format=None):
@@ -18,28 +47,6 @@ def api_root(request, format=None):
         'projects': reverse('project-list', request=request, format=format)
     })
 
-class ProjectHighlight(generics.GenericAPIView):
-    queryset = Project.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
-
-    def get(self, request, *args, **kwargs):
-        project = self.get_object()
-        return Response(project.description)
-
-class ProjectList(generics.ListAPIView):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
-
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -47,4 +54,4 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+"""
