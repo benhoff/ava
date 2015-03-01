f26abaa0301c73096150dbfe45bc33c3cd37d732
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-03-01 16:33:33 -0600

    cleaned up some of the import cruft, as well as a spelling error

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from projects import views
 from rest_framework.routers import DefaultRouter
 
@@ -8,8 +7,8 @@ router.register(r'projects', views.ProjectViewSet)
 router.register(r'users', views.UserViewSet)
 
 # API endpoints
-urlpatterns = format_suffix_patterns([
+urlpatterns = [
         url(r'^$', include(router.urls)),
 
         url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-])
+]
--- projects/views.py
+++ projects/views.py
@@ -1,16 +1,11 @@
 from projects.models import Project
 from projects.serializers import ProjectSerializer, UserSerializer
 from projects.permissions import IsOwnerOrReadOnly
-from rest_framework import mixins
-from rest_framework import generics
-from rest_framework import renderers
 from django.contrib.auth.models import User
-from rest_framework import permissions
-from rest_framework.decorators import api_view
+from rest_framework import permissions, viewsets, renderers
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
-from rest_framework import viewsets
 from rest_framework.decorators import detail_route
+
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This viewset automagically provides 'list' and 'detail' actions.
@@ -30,28 +25,10 @@ class ProjectViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
 
-    @detail_route(render_classes=[renderers.StaticHTMLRenderer])
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         project = self.get_object()
         return Response(project.description)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-"""
-
-@api_view(('GET',))
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'projects': reverse('project-list', request=request, format=format)
-    })
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-"""
