f755b700cfe5c39df1d0f82a5208b3ddb2172a5c
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-04-25 08:16:37 -0500

    moved the miscallenious serializers out of the proejct serializer

--- projects/serializers.py
+++ projects/serializers.py
@@ -5,16 +5,10 @@ from rest_extensions.relations import HyperlinkedNestedRelatedField
 from projects.models import Project
 from ideas.models import Idea
 
-# TODO: Think about how we really want to get at this information
-from ideas.serializers import IdeaSerializer
-from discussions.serializers import DiscussionSerializer
 
 class ProjectSerializer(serializers.ModelSerializer):
     ownername = serializers.ReadOnlyField(source='owner.username')
 
-    # Is this better suited to use as a 'edge'
-    ideas = IdeaSerializer(many=True)
-    discussions = DiscussionSerializer(many=True)
     class Meta:
         model = Project
         fields = (
@@ -24,6 +18,4 @@ class ProjectSerializer(serializers.ModelSerializer):
                 'description', 
                 'status', 
                 'ownername', 
-                'owner',
-                'ideas',
-                'discussions')
+                'owner')
