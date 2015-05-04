2711eefc1412400e4091eadfee2ed99fcf4425d9
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-05-04 08:29:25 -0500

    need to make wiki more flexible, like comments

--- wiki/models.py
+++ wiki/models.py
@@ -1,18 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 
 class ArticleRevision(models.Model):
     article = models.ForeignKey('Article', on_delete=models.CASCADE)
     content = models.TextField(blank=True)
     title = models.CharField(max_length=255, null=False, blank=False)
     revision_number = models.IntegerField(editable=False)
-    owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey('auth.User', blank=True, null=True, on_delete=models.SET_NULL)
     edited = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     previous_revision = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
     locked = models.BooleanField(default=False)
 
-
+# TODO: add in comment like generic types.
 class Article(models.Model):
     current_revision = models.OneToOneField('ArticleRevision', verbose_name='current revision',
                                             blank=True, null=True, related_name='current_set')
