8136d0ad5a386e0cbefca4b5cff20ce233960356
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-05-04 05:46:45 -0500

    started hacking on wiki models. Unfinished. Untested

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ArticleRevision(models.Model):
+    article = models.ForeignKey('Article', on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    revision_number = models.IntegerField(editable=False)
+    owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    edited = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    previous_revision = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
+    locked = models.BooleanField(default=False)
+
+
+class Article(models.Model):
+    current_revision = models.OneToOneField('ArticleRevision', verbose_name='current revision',
+                                            blank=True, null=True, related_name='current_set')
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, blank=True, 
+                              null=True, related_name='owned_articles', 
+                              on_delete=models.SET_NULL)
+
+    # Group? group read(boolean field)? group write (boolean field)?
